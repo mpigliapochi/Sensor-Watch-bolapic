de2ca0684849a77ad60b51b2ad8758a9d9747be5
mpigliapochi <39917152+mpigliapochi@users.noreply.github.com>
2024-05-07 23:45:36 +0200

    Update movement_config.h

--- movement/movement_config.h
+++ movement/movement_config.h
@@ -35,7 +35,7 @@ const watch_face_t watch_faces[] = {
   alarm_face,
   sunrise_sunset_face,
   moon_phase_face,
-  thermistor_readout_secret_face,
+  thermistor_readout_face,
   probability_face,
   wyoscan_face,
   set_time_face,
@@ -54,7 +54,7 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 2) // or (0)
+#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 8) // or (0)
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options. */
 #define SIGNAL_TUNE_DEFAULT
